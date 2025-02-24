2378213b49e1f9bbdb5a52a6f562b4ec930998f5
William Anderson <andersonwj@gmail.com>
2025-02-24 15:56:35 -0700

    adds call to menuItem.json and flatlist logic

--- parsing-data/app/index.tsx
+++ parsing-data/app/index.tsx
@@ -1,15 +1,58 @@
-import { Text, View } from "react-native";
+//import { Text, View } from "react-native";
+import React from 'react';
+import { FlatList, Text, SafeAreaView, View, StyleSheet } from 'react-native';
+import menuItems from '../menuItems.json'
+
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+  
+  const menuData = menuItems.menu;
+
+  const Item = ({name , price})=>(
+    <View style={menuStyles.innerContainer}>
+    <Text style={menuStyles.itemText}>{name}</Text>
+    <Text style={menuStyles.itemText}>{'$' + price}</Text>
+  </View>
+  )
+
+  const renderItem = ({item})=> (
+    <Item name={item.title} price={item.price}/>
+
   );
-}
+
+
+  return (
+    <SafeAreaView style={menuStyles.container}>
+      <Text style={menuStyles.headerText}>Little Lemon Menu</Text>
+      <FlatList
+      data={menuData}
+      keyExtractor={({id}, Index)=>id}
+      renderItem={renderItem}
+      
+      
+      />
+    </SafeAreaView>
+  );
+};
+
+const menuStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  innerContainer: {
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    backgroundColor: '#495E57',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  itemText: {
+    color: '#F4CE14',
+    fontSize: 22,
+  },
+  headerText: {
+    color: '#495E57',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
